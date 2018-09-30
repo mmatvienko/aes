1ba6a182a0d3b7b012b3cc42a66cebb404808f22
Marc Matvienko <m.matvienko@utexas.edu>
2018-09-30 15:21:09 -0500

    starting to deal with blocks and padding

--- main.py
+++ main.py
@@ -4,18 +4,34 @@ Nb = 4
 Nk = 4
 Nr = 10
 
+# aes 256
+Nk = 8
+Nr = 14
+
 inp = None
-with open('input', 'rb') as f:
+with open('inputfile', 'rb') as f:
     inp = f.read()
+    length = 16 - (len(inp) % 16)
+    inp += bytes([length])*length
 
 key = None
-with open('key', 'rb') as f:
+with open('keyfile', 'rb') as f:
     key = f.read()
+w = [int(x) for x in KeyExpansion(key, Nk, Nb, Nr)]
+print(w)
 
-w = [hex(x) for x in KeyExpansion(key, Nk, Nb, Nr)]
 
-def toByteBlock(x):
-    return [hex(x) for x in inp]
+def toByteBlock(y):
+    return [x for x in y]
 
-print(Cipher(toByteBlock(inp),w,Nb,Nr))
-# input is strig for some reason in AddRoundKey. fix that
+final = []
+
+for x in range():
+    part = [hex(x) for x in Cipher(toByteBlock(inp[16*c:16*(c+1)]),w,Nb,Nr)]
+    final.append(part)
+print([hex(x) for x in Cipher(toByteBlock(inp[0:16]),w,Nb,Nr)])
+print([hex(x) for x in Cipher(toByteBlock(inp[16:32]),w,Nb,Nr)])
+
+with open('outputfile', 'rb') as f:
+    out = f.read()
+    print([hex(x) for x in out])
